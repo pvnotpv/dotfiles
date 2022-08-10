86714bf265679ef842920a1dedc3f318bf9a9d24
dumbusername <66077878+doubledickdude@users.noreply.github.com>
2022-08-10 19:24:52 +0530

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,8 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar stuffs 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar misc 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
